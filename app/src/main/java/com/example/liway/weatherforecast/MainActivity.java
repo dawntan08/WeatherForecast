169331292f403117d2894b8bf605f293d7386580
meshlines <parasatumblr@gmail.com>
2015-06-20 18:19:34 +0800

    commit ni mess

--- app/src/main/java/com/example/liway/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/liway/weatherforecast/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //random comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
